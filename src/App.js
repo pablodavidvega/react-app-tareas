9a5d4ecfc0e629d1f3c40d9348176602342ae55c
Pabloseedem <pavega@seedem.co>
2024-01-02 16:52:19 -0500

    Subir proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import logoDigitalShopping from './Imagenes/logoD.jpeg'
+import ListaDeTareas from './Componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='frecodecamp-logo-contenedor'>
+        <img 
+          src={ logoDigitalShopping }
+          className='freecodecamp-logo' 
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
